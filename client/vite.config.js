93ad25fa99f39b16d7d6fbe8092dc7818dd811b6
Frank Meies <19324766+fmeies@users.noreply.github.com>
2026-04-07 21:53:02 +0200

    fix baseurl

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/music-quiz',
+  base: '/music-quiz/',
   server: {
     port: 3010,
     proxy: {
